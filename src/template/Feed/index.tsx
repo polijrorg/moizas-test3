7edd72bd6b5169068ffa2c511b3c584b77054e5b
Moizas951 <moizesavila@usp.br>
2025-04-20 04:05:22 -0300

    Refactor code structure for improved readability and maintainability

--- src/template/Feed/index.tsx
+++ src/template/Feed/index.tsx
@@ -5,7 +5,7 @@ const ExampleTemplate = () => {
     return (
         <S.Container>
             <S.ContentWrapper>
-                <S.Title>Treinamento Introdutório de Front</S.Title>
+                <S.Title>VAI SE FODER PORRAQQQQQAUAAIAMAMAMAMM</S.Title>
                 <S.Comment>Template criado em Abril de 2025</S.Comment>
                 <S.Description>Template criado por Moizés Ávila</S.Description>
             </S.ContentWrapper>
